4ac7ba99598aeced1c63a08b5a62c5ca6e5d3128
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2023-09-14 09:28:17 -0300

    Exercicio 2

--- src/tests/modal.test.js
+++ src/tests/modal.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event";
 import Modal from "../components/Modal";
 
@@ -32,4 +32,31 @@ const closeModalMock = jest.fn()
 
 describe("Testando o modal", () => {
 
+    test("Deve renderizar os elementos do modal", () => {
+
+        render(
+            <Modal activeModal={activeModalMock} closeModal={closeModalMock}/>
+        )
+
+        const urlImage = screen.getByRole('img', {name: /bulbasaur/i})
+        const id = screen.getByRole('heading', {name: /#1 bulbasaur/i})
+        const name = screen.getByText(/bulbasaur/i)
+        const typeGrass = screen.getByText(/grass/i)
+        const typePoison = screen.getByText(/poison/i)
+        const weight = screen.getByText(/6\.9 kg/i)
+        const height = screen.getByText(/0\.7 m/i)
+        const buttonClose = screen.getByRole('button', {name: /❌/i})
+
+
+        expect(urlImage).toBeInTheDocument()
+        expect(id).toBeInTheDocument()
+        expect(name).toBeInTheDocument()
+        expect(typeGrass).toBeInTheDocument()
+        expect(typePoison).toBeInTheDocument()
+        expect(weight).toBeInTheDocument()
+        expect(height).toBeInTheDocument()
+        expect(buttonClose).toBeInTheDocument()
+
+        screen.logTestingPlaygroundURL()
+    })
 })
